81e1628c61d16cb41a8c11f9195331e5edfc2fe3
Kurozana <47218800+Ibrahimalboiez@users.noreply.github.com>
2022-08-15 20:37:12 +0300

    Update botmain.java

--- botmain.java
+++ botmain.java
@@ -1,28 +1,41 @@
 package Bot;
+import Bot.Listeners.EventListener;
 import io.github.cdimascio.dotenv.Dotenv;
-import kotlin.random.Random;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.sharding.DefaultShardManager;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.sharding.ShardManager;
-
 import javax.security.auth.login.LoginException;
-import java.util.EventListener;
-import java.util.Locale;
+
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 
 public class BotMain {
     private final Dotenv config;
     private final ShardManager SM;
+
     public BotMain() throws LoginException{
-         config = Dotenv.configure().load();
+        config = Dotenv.configure().load();
         String token =  config.get("TOKEN");
+
+        // Builder & Shard Manager
         DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
-        builder.setStatus(OnlineStatus.ONLINE);
+        builder.setStatus(OnlineStatus.DO_NOT_DISTURB);
         builder.setActivity(Activity.watching("Femboys dance"));
         SM = builder.build();
+        builder.enableIntents(GatewayIntent.MESSAGE_CONTENT);
+        builder.setMemberCachePolicy(MemberCachePolicy.ALL);
+        builder.setChunkingFilter(ChunkingFilter.ALL);
+        builder.enableCache(CacheFlag.ACTIVITY, CacheFlag.MEMBER_OVERRIDES, CacheFlag.ONLINE_STATUS);
 
+
+
+
+        // Register Listeners here :)
+        SM.addEventListener(new EventListener());
 
     }
 
