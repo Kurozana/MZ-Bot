90cdd9ec1cee23a303f9443a597ee93de615072e
Kurozana <47218800+Ibrahimalboiez@users.noreply.github.com>
2022-08-16 14:13:46 +0300

    Update WorkBot.java

--- WorkBot.java
+++ WorkBot.java
@@ -9,24 +9,27 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.sharding.DefaultShardManager;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.sharding.ShardManager;
-
 import javax.security.auth.login.LoginException;
 import java.util.Locale;
 
 
 public class BotMain {
     // Defining finals + calling config.
+    public static String Prefix = ".";
     private final Dotenv config;
     private final ShardManager SM;
     public BotMain() throws LoginException{
     config = Dotenv.configure().load();
     String token =  config.get("TOKEN");
+    String Prefix = config.get("PREFIX");
+
 
         // Building the ShardManager
         DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
         builder.setStatus(OnlineStatus.ONLINE);
         builder.setActivity(Activity.watching("Femboys dance"));
         builder.enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MESSAGE_TYPING, GatewayIntent.GUILD_MEMBERS);
+
         SM = builder.build();
         SM.addEventListener(new Actions());
         SM.addEventListener(new MJ());
